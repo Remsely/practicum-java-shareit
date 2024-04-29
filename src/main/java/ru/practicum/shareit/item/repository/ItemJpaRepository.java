eed88cb68e498cdf8c4d57335f47d7c7e52930c1
Remsely <prokopchukromain@mail.ru>
2024-04-29 17:57:25 +0300

    feat: добавить ItemJpaRepository для хранения данных в PostgreSQL

--- src/main/java/ru/practicum/shareit/item/repository/ItemJpaRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemJpaRepository.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.List;
 
 public interface ItemJpaRepository extends JpaRepository<Item, Long> {
+    List<Item> findByOwner(User owner);
+
+    @Query(" select i from Item i " +
+            "where i.available = true and (lower(i.name) like lower(concat('%', ?1, '%'))" +
+            "    or lower(i.description) like lower(concat('%', ?1, '%')))")
+    List<Item> search(String query);
 }
--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -3,9 +3,13 @@ package ru.practicum.shareit.item.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.exception.EntityNotFoundException;
+import ru.practicum.shareit.exception.UserIdWithoutAccessRightsException;
+import ru.practicum.shareit.exception.model.ErrorResponse;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.item.repository.ItemRepository;
-import ru.practicum.shareit.user.repository.UserRepository;
+import ru.practicum.shareit.item.repository.ItemJpaRepository;
+import ru.practicum.shareit.user.repository.UserJpaRepository;
 
 import java.util.List;
 
@@ -13,52 +17,113 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
-    private final ItemRepository itemRepository;
-    private final UserRepository userRepository;
+    private final ItemJpaRepository itemRepository;
+    private final UserJpaRepository userRepository;
 
+    @Transactional
     @Override
     public Item addItem(Item item, long userId) {
-        userRepository.checkUserExistence(userId);
-        item.setOwner(userRepository.get(userId));
+        item.setOwner(userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException(
+                ErrorResponse.builder()
+                        .reason("User repository")
+                        .message("User with id " + userId + " does not exist!")
+                        .build()
+        )));
 
-        Item addedItem = itemRepository.add(item);
+        Item addedItem = itemRepository.save(item);
         log.info("add Item: an item with an id {} and owner id {} has been added. Item : {}.",
                 addedItem.getId(), userId, addedItem);
         return addedItem;
     }
 
+    @Transactional
     @Override
     public Item updateItem(Item item, long itemId, long userId) {
-        userRepository.checkUserExistence(userId);
-        item.setOwner(userRepository.get(userId));
-        item.setId(itemId);
+        item.setOwner(userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException(
+                ErrorResponse.builder()
+                        .reason("User repository")
+                        .message("User with id " + userId + " does not exist!")
+                        .build()
+        )));
+        Item itemToUpdate = itemRepository.findById(itemId).orElseThrow(() -> new EntityNotFoundException(
+                ErrorResponse.builder()
+                        .reason("Item repository")
+                        .message("Item with id " + itemId + " already exist!")
+                        .build()
+        ));
+        checkPermission(userId, itemToUpdate);
+        updateNonNullProperties(itemToUpdate, item);
 
-        Item updatedItem = itemRepository.update(item);
+        Item updatedItem = itemRepository.save(itemToUpdate);
         log.info("update Item: an item with an id {} and owner id {} has been updated. Item : {}.",
                 updatedItem.getId(), userId, updatedItem);
         return updatedItem;
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Item getItem(long id) {
-        Item item = itemRepository.get(id);
+        Item item = itemRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(
+                ErrorResponse.builder()
+                        .reason("Item repository")
+                        .message("Item with id " + id + " already exist!")
+                        .build()
+        ));
         log.info("get Item: a item with an id {} has been received. Item : {}.", item.getId(), item);
         return item;
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Item> getUserItems(long userId) {
-        userRepository.checkUserExistence(userId);
-        List<Item> items = itemRepository.getByUserId(userId);
+        List<Item> items = itemRepository.findByOwner(userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        ErrorResponse.builder()
+                                .reason("User repository")
+                                .message("User with id " + userId + " does not exist!")
+                                .build()
+                )));
         log.info("get user's Items: the list of items of the user with id {} has been received. List : {}.",
                 userId, items);
         return items;
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Item> searchItems(String query) {
         List<Item> items = itemRepository.search(query);
         log.info("The list of items requested by query \"{}\" has been received. List {}.", query, items);
         return items;
+    }
+
+    private void checkUserExistence(Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new EntityNotFoundException(ErrorResponse.builder()
+                    .reason("User repository")
+                    .message("User with id " + id + " does not exist!")
+                    .build()
+            );
+        }
+    }
+
+    private void updateNonNullProperties(Item existingItem, Item newItem) {
+        if (newItem.getName() != null) {
+            existingItem.setName(newItem.getName());
+        }
+        if (newItem.getDescription() != null) {
+            existingItem.setDescription(newItem.getDescription());
+        }
+        if (newItem.getAvailable() != null) {
+            existingItem.setAvailable(newItem.getAvailable());
+        }
+    }
+
+    private void checkPermission(long userId, Item item) {
+        if (!item.getOwner().getId().equals(userId)) {
+            throw new UserIdWithoutAccessRightsException(ErrorResponse.builder()
+                    .reason("Forbidden for this id")
+                    .message("The user with id " + userId + " does not have access to this item")
+                    .build());
+        }
     }
 }
