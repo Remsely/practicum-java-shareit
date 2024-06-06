649d6decf62874f4632631337413f961e2a09197
Remsely <prokopchukromain@mail.ru>
2024-06-07 01:44:49 +0300

    fix: unused imports

--- server/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Data
--- server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit.user.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-
 @Data
 @Builder
 public class UserDto {
