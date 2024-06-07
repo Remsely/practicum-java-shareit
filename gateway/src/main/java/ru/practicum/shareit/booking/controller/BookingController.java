553fba32ea3176d3b4b17562fc9058c2ac2eaa56
Remsely <prokopchukromain@mail.ru>
2024-06-07 20:34:04 +0300

    refactor: вынести логику валидации из BookingController в отдельный компонент

--- gateway/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -4,16 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.booking.service.BookingClient;
 import ru.practicum.shareit.booking.dto.BookingCreationDto;
 import ru.practicum.shareit.booking.dto.BookingState;
-import ru.practicum.shareit.common.exception.DatesValidationException;
-import ru.practicum.shareit.common.exception.ErrorResponse;
-import ru.practicum.shareit.common.exception.UnsupportedStateException;
+import ru.practicum.shareit.booking.service.BookingClient;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -21,12 +16,13 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class BookingController {
     private final BookingClient client;
+    private final BookingValidator validator;
 
     @PostMapping
     public ResponseEntity<?> postBooking(@Valid @RequestBody BookingCreationDto bookingDto,
                                          @RequestHeader("X-Sharer-User-id") Long userId) {
         log.info("POST /bookings (X-Sharer-User-id = {}). Request body : {}", userId, bookingDto);
-        validateDates(bookingDto.getStart(), bookingDto.getEnd());
+        validator.validateDates(bookingDto.getStart(), bookingDto.getEnd());
         return client.postBooking(bookingDto, userId);
     }
 
@@ -50,7 +46,7 @@ public class BookingController {
                                              @RequestParam(required = false) Integer from,
                                              @RequestParam(required = false) Integer size) {
         log.info("GET /bookings?state={}&from={}&size={} (X-Sharer-User-id = {})", state, from, size, userId);
-        BookingState bookingState = validateBookingState(state);
+        BookingState bookingState = validator.validateBookingState(state);
         return client.getUserBookings(userId, bookingState, from, size);
     }
 
@@ -60,36 +56,7 @@ public class BookingController {
                                                   @RequestParam(required = false) Integer from,
                                                   @RequestParam(required = false) Integer size) {
         log.info("GET /bookings/owner?state={}&from={}&size={} (X-Sharer-User-id = {})", state, from, size, userId);
-        BookingState bookingState = validateBookingState(state);
+        BookingState bookingState = validator.validateBookingState(state);
         return client.getUserItemsBookings(userId, bookingState, from, size);
-    }
-
-    private void validateDates(LocalDateTime from, LocalDateTime to) {
-        if (to.isBefore(from)) {
-            throw new DatesValidationException(ErrorResponse.builder()
-                    .reason("Booking dates")
-                    .error("The end date can't be earlier than the start date!")
-                    .build()
-            );
-        }
-        if (to.isEqual(from)) {
-            throw new DatesValidationException(ErrorResponse.builder()
-                    .reason("Booking dates")
-                    .error("The end date can't be equal to the start date!")
-                    .build()
-            );
-        }
-    }
-
-    private BookingState validateBookingState(String state) {
-        Optional<BookingState> stateOptional = BookingState.fromString(state);
-        if (stateOptional.isPresent()) {
-            return stateOptional.get();
-        }
-        throw new UnsupportedStateException(ErrorResponse.builder()
-                .reason("State parameter")
-                .error("Unknown state: " + state)
-                .build()
-        );
     }
 }
